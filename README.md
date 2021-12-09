88b4555aa1880d2395d063ecf0c03691bded6339
Simon Possegger <simon.possegger@student.tugraz.at>
2021-12-09 22:46:45 +0100

    Restructure argparsing, add testcases

--- README.md
+++ README.md
@@ -1,5 +1,67 @@
+# Alan.Python
+
 A small hack to run exports from
 
 https://focs.ist.tugraz.at/turingmachine/
 
+# Usage
+Print help
+```bash
+python3 tu.py -h
+usage: tu.py [-h] --json JSON [--debug] [--tape] [--testcases {4,6,8}]
 
+Turing machine test-script by Daef (modified by Chr0x6eOs).
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --json JSON, -j JSON  Json file containing Turing machine settings
+  --debug, -d           Turn on debug
+  --tape , -t           Tape to test
+  --testcases {4,6,8}, -tc {4,6,8}
+                        Amount of bits create possible testcases for addition
+```
+
+## Tape
+
+You can supply a tape to test using the `--tape` flag:
+```bash
+python3 tu.py --json addition.json --tape "1010+0101" | tail -n 2
+_1111______
+ ^- End
+```
+
+## Testcases
+
+You can specify how many bit additions to test:
+```bash
+python3 tu.py --json addition.json --testcases 4
+You are about to run 16 testcases! Continue? [Y/n] 
+[1/16] Running testcase: 0000+0000:
+...
+_0000______
+ ^- End
+[2/16] Running testcase: 0001+0001:
+  ...
+_0010______
+^- End
+  ...
+[16/16] Running testcase: 1111+1111:
+  ...
+_0000
+^- Overflow
+```
+
+If you combine `--tape` with `--testcases` you can run a further testcase:
+```bash
+python3 tu.py --json addition.json --testcases 4 --tape "101011+00011"
+You are about to run 17 testcases! Continue? [Y/n] 
+[1/17] Running testcase: 101011+00011:
+...
+_101110_______
+ ^- End
+  ...
+[17/17] Running testcase: 1111+1111:
+  ...
+_0000
+^- Overflow
+```
--- tu.py
+++ tu.py
@@ -1,63 +1,116 @@
 #!/usr/bin/env python3
 
-import json, sys
+# Credit: daef
+# Modified by Chr0x6eOs
+
+import json, sys, argparse, itertools
 
 last_ts = ''
 def print_tape(tape, only_if_new):
-    global last_ts
-    ts = ''.join(tape)
-    if not only_if_new or last_ts != ts:
-        print(ts)
-    last_ts = ts
+  global last_ts
+  ts = ''.join(tape)
+  if not only_if_new or last_ts != ts:
+      print(ts)
+  last_ts = ts
 
 def print_cursor(cur, state):
-    print(f'{" "*cur}^- {state}')
+  print(f'{" "*cur}^- {state}')
 
-def usage(args):
-    print(f'usage: {args[0]} my_awesome_touring_ding.json [debug] [tape]')
-    exit(1)
+def run_testcases(json_data, dbg, bit_count, tape=""):
+  testcases = []
+  binary_num = []
+
+  for count in range(2, bit_count + 1):
+    binary_num.append(["".join(i) for i in itertools.product(["0", "1"], repeat=count)])
+
+  for binary_range in binary_num:
+    for binary in ["".join(i) for i in itertools.product(binary_range, repeat=2)]:
+      testcases.append(f"{binary}+{binary}")
+
+  if tape != "":
+    testcases.insert(0, tape)
+
+  count = 0
+
+  run = input(f"You are about to run {len(testcases)} testcases! Continue? [Y/n] ").lower()
+  if(run in ["no","n"]):
+    print("OK! Stopping...")
+    return
+
+  for testcase in testcases:
+    count += 1
+    print(f"[{count}/{len(testcases)}] Running testcase: {testcase}:\n")
+    solve(json_data, tape=testcase)
+
+def solve(json_data, dbg=False, tape = ""):
+  results = []
+
+  init = json_data['initial_tape']
+  null = init['blank_symbol']
+  if tape != "":
+    tape = [c for c in tape]
+    cur = len(tape)
+    tape.append(null)
+  else:
+      tape = init['data']
+      cur = init['cursor']
+  state = json_data['initial_state']
+  fin = json_data['final_states']
+  prg = json_data['program']
+
+  while not state in fin:
+    r = tape[cur]
+    matches = [x for x in prg if x[0] == r and x[1] == state]
+    print_tape(tape, not dbg)
+    if dbg:
+        results.append(print_cursor(cur, state))
+    if len(matches) != 1:
+        print(f'{len(matches)} rules matching ({matches})')
+        exit(1)
+    m = matches[0][2]
+    tape[cur] = m[0]
+    if m[1] == 'Left':
+        cur -= 1
+        while cur < 0:
+            tape.insert(0, null)
+            cur += 1
+    elif m[1] == 'Right':
+        cur += 1
+    state = m[2]
+  results.append(print_cursor(cur, state))
+  return results
+
+def log_results(results):
+  pass
 
 def main(args):
-    argl = len(args)
-    if argl < 2:
-        usage(args)
-    j = json.load(open(args[1]))
+  parser = argparse.ArgumentParser(description='Turing machine test-script by Daef (modified by Chr0x6eOs).')
+  parser.add_argument('--json', '-j', required=True, help='Json file containing Turing machine settings', type=str)
+  parser.add_argument('--debug', '-d', help='Turn on debug', default=False, action='store_true')
+  parser.add_argument('--tape', '-t', help='Tape to test', metavar="", type=str)
+  parser.add_argument('--testcases', '-tc', help='Amount of bits create possible testcases for addition', choices=[4, 6, 8], type=int)
+  args = parser.parse_args()
 
-    dbg = argl > 2 and args[2] == 'debug'
-    init = j['initial_tape']
-    null = init['blank_symbol']
-    if argl > 3 or (not dbg and argl > 2):
-        tape = [c for c in args[dbg and 3 or 2]]
-        cur = len(tape)
-        tape.append(null)
-    else:
-        tape = init['data']
-        cur = init['cursor']
-    state = j['initial_state']
-    fin = j['final_states']
-    prg = j['program']
-    while not state in fin:
-        r = tape[cur]
-        matches = [x for x in prg if x[0] == r and x[1] == state]
-        print_tape(tape, not dbg)
-        if dbg:
-            print_cursor(cur, state)
-        if len(matches) != 1:
-            print(f'{len(matches)} rules matching ({matches})')
-            exit(1)
-        m = matches[0][2]
-        tape[cur] = m[0]
-        if m[1] == 'Left':
-            cur -= 1
-            while cur < 0:
-                tape.insert(0, null)
-                cur += 1
-        elif m[1] == 'Right':
-            cur += 1
-        state = m[2]
-    print_cursor(cur, state)
+  if not any(vars(args).values()):
+      parser.print_help()
+      exit(1)
 
+  json_data = None
+  tape = ""
+
+  try:
+    json_data = json.load(open(args.json))
+  except Exception as ex:
+    print(f"[-] Error while loading json: {ex}")
+    exit(1)
+
+  if args.tape:
+    tape = args.tape
+
+  if args.testcases:
+    run_testcases(json_data, args.debug, int(args.testcases / 2), tape)
+  else:
+    solve(json_data, args.debug, tape)
 
 if __name__ == "__main__":
     main(sys.argv)
-
